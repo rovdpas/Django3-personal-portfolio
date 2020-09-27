b63d40a26f66009c05fd4b4f2daf17f10a37efa3
Ron <ropa@live.nl>
2020-09-27 19:03:30 +0000

    Verwijder oude bestanden

--- pers_port/settings.py
+++ pers_port/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'hptg4b=pknjo%ta1r5_996u$1giz&qad9u+d=^jv546f&rc2*e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rovdpas.pythonanywhere.com']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
