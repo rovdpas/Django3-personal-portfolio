bc10ba65caffbdde6fb5f96d4a1ba84860499830
RonvdPas <ropa@live.nl>
2020-09-27 21:22:04 +0200

    lokale settings gewijzigd

--- pers_port/settings.py
+++ pers_port/settings.py
@@ -126,3 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Geen lokale settings gevonden dus je bent op productie")
